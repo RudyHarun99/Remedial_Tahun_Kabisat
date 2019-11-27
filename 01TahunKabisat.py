a221cc5cd49f0652ecb6495bee20227a641a9b17
RudyHarunArrasyid <rdyhrn@gmail.com>
2019-11-27 13:22:46 +0700

    kabisat

---
+++ 01TahunKabisat.py
@@ -0,0 +1,8 @@
+def kabisat():
+    tahun=int(input('Input Tahun : '))
+    if tahun%4==0:
+        print('Hasil : Tahun Kabisat')
+    else:
+        print('Hasil : Bukan Tahun Kabisat')
+
+kabisat()
